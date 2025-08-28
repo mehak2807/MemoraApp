110ed57b8ce470a6c79ed85033bba8df05a70baa
Mehak Taneja <mehaktaneja2807@gmail.com>
2025-08-29 00:15:24 +0530

    Add authentication middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const Caregiver = require('../models/Caregiver');
+
+const protect = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.caregiver = await Caregiver.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  } else {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+};
+
+module.exports = { protect };
