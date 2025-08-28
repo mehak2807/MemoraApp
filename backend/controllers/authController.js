88c5e7806df5f7cce5fe1ffec9b31d621a838f14
Mehak Taneja <mehaktaneja2807@gmail.com>
2025-08-29 00:07:48 +0530

    Add authController.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,10 @@
+// authController.js
+// Controller for authentication-related actions
+
+exports.login = (req, res) => {
+    // Handle user login
+};
+
+exports.register = (req, res) => {
+    // Handle user registration
+};
